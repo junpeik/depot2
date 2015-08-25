a3d224ca6df15dd2a51d2508e9040c414df32819
Junpei <kusumoto.0513.jp@gmail.com>
2015-08-25 22:57:28 +0900

    finish chapter9

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_items
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
@@ -6,4 +8,15 @@ class Product < ActiveRecord::Base
     with: /\.(gif|jpg|png)\z/i,
     message: 'はGIF,JPG,PNG画像のURLでなければなりません'
   }
+
+  private
+
+    def ensure_not_referenced_by_any_line_items
+      if line_items.empty?
+        return true
+      else
+        errord.add(:base, '品目が存在します')
+        return false
+      end
+    end
 end
