de6cb0d1ac6e33b91edfe68aed11c0f0060c429b
Junpei <kusumoto.0513.jp@gmail.com>
2015-09-02 22:13:15 +0900

    finish chapter14

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store'
+  get 'store/index'
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
-  root 'store#index'
-  get 'store/index', as: 'store'
-
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
